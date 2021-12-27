f2b9b553dfe53b7691c18f08ced45c846c766d26
bmarcos <bmarcos81@gmail.com>
2021-12-27 22:25:35 +0100

    Updates

---
+++ precache-manifest.fdeec80e209db273954691139bd5dd7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a388e069628bb1c1a5762503a0d6f3",
+    "url": "/intro-react/index.html"
+  },
+  {
+    "revision": "612f9756c9b01842f946",
+    "url": "/intro-react/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/intro-react/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "612f9756c9b01842f946",
+    "url": "/intro-react/static/js/main.3957455f.chunk.js"
+  },
+  {
+    "revision": "aa8e1f74143b90d1364a",
+    "url": "/intro-react/static/js/runtime~main.37ae2610.js"
+  }
+]);
